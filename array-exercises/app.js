14205aa475b5facc539acb9449438560bb2d762b
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-17 15:43:16 +0100

    Update app.js

--- array-exercises/app.js
+++ array-exercises/app.js
@@ -1,4 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
 
 // loop through array numbers
 for (let index = 0; index < numbers.length; index++) {
@@ -17,3 +18,72 @@ for (let index = numbers.length - 1; index >= 0; index--) {
 
 // or you could simply use numbers.reverse();
 // before looping in regular order.
+
+const mix = [2, 4, "Hello", 3, "Hi"];
+let antalTal = 0;
+let antalStrings = 0;
+
+for (let i = 0; i < mix.length; i++) {
+    console.log(mix[i]);
+
+    if (Number.isInteger(mix[i])) {
+        console.log("Er et tal");
+        antalTal++;
+    } else {
+        console.log("Er en string");
+        antalStrings++;
+    }
+}
+
+console.log("tal:", antalTal);
+console.log("strings:", antalStrings);
+
+if (antalTal > antalStrings) {
+    console.log("Most integers");
+} else if (antalStrings > antalTal) {
+    console.log("Most strings");
+}
+
+// Exercise 1
+// Write an array with 3 strings
+// Using foreach - print each string
+const strings = ["Hello", "Hi", "Daws"];
+
+strings.forEach(function (item) {
+    console.log(item);
+});
+
+strings.forEach(item => {
+    console.log(item);
+});
+
+// Exercise 2
+// Write an array with 5 numbers
+// Using foreach - write a program that sums the numbers
+const numbers2 = [44, 88, 99, 3, 44];
+let sum = 0;
+numbers2.forEach(number => {
+    sum = sum + number;
+});
+console.log(sum);
+
+// Exercise 3
+// Write an array with 5 numbers
+// Using foreach - write a program that calculates the average of the numbers
+const average = sum / numbers.length;
+console.log(average);
+
+// Write an array with 5 numbers - some larger than zero some smaller
+// Using the filter function of the array
+// create a new array with all values larger than 0
+// Print and verify the result
+const numbers3 = [1, 0, 3, 6, 11, -7, -2];
+const results = numbers3.filter(number => number > 0);
+console.log(results);
+
+let count = 0;
+
+setInterval(() => {
+    count++;
+    document.write(count);
+}, 1000);
